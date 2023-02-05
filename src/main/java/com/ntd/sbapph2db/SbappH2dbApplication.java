d14e8e356b22de740f1bd8cef651a4e8395c2587
nthanhdo2610 <nthanhdo11110030@gmail.com>
2023-02-05 13:09:03 -0600

    init project

---
+++ src/main/java/com/ntd/sbapph2db/SbappH2dbApplication.java
@@ -0,0 +1,13 @@
+package com.ntd.sbapph2db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbappH2dbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbappH2dbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ntd/sbapph2db/SbappH2dbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntd.sbapph2db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbappH2dbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
